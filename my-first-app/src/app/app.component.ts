aba9a32e632f48b327fb60686e9f3b0f52f9b320
Dmitry Vlasov <dime.vlasov@gmail.com>
2017-09-28 22:10:02 +0500

    My first components
    Test command 'ng g c servers'

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -6,6 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = '"This is my first change in angular application! This text changed without restart application!"';
-  name = '';
 }
