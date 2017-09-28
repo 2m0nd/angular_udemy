65e15600aa4617d700b5a4420e5b9ba420fca96c
Dmitry Vlasov <dime.vlasov@gmail.com>
2017-09-29 00:04:49 +0500

    Practicing 3

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -7,7 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   username = '';
+  items = [];
   cleanupUsername() {
     this.username = '';
+  }
+  addItems() {
+    this.items.push(this.items.length + 1);
+    console.log(this.items);
+  }
+  getColor(item: number) {
+    return item >= 5 ? 'blue' : 'transperent';
   }
 }
