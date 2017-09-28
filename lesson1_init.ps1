b9073505be4f68b974013df8e96ad77ae73b381f
Dmitry Vlasov <dime.vlasov@gmail.com>
2017-09-28 21:12:30 +0500

    Add lesson 1 steps

---
+++ lesson1_init.ps1
@@ -0,0 +1,17 @@
+#update npm
+npm isntall -g npm
+
+#clean cache
+npm cache clean
+
+#install angular cli
+npm install -g @angular/cli
+
+#create angular application from template
+ng new my-first-app
+
+#go to app dir
+cd my-first-app
+
+#run app
+ng serve
