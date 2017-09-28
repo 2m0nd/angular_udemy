6fb4a52a50e724682a6f617179c1908d0bd0ddb2
Dmitry Vlasov <dime.vlasov@gmail.com>
2017-09-28 21:16:10 +0500

    lesson 2 change 1

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = '"This is my first change in angular application! This text changed without restart application!"';
 }
