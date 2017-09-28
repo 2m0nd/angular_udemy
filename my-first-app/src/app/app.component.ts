965547fe48b7c73e5f4c592d9af724dd757ff0bd
Dmitry Vlasov <dime.vlasov@gmail.com>
2017-09-28 21:21:19 +0500

    lesson 2 change 2

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = '"This is my first change in angular application! This text changed without restart application!"';
+  name = '';
 }
