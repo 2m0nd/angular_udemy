9c8d6810a30b6fa06935a8c800202f9a6f830365
Dmitry Vlasov <dime.vlasov@gmail.com>
2017-11-11 01:53:08 +0500

    Solved #7

--- forms-reactive-assignment-start/src/app/app.component.ts
+++ forms-reactive-assignment-start/src/app/app.component.ts
@@ -1,9 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  myGroup: FormGroup;
+  statuses = ['Stable', 'Critical', 'Finished'];
+
+  ngOnInit() {
+    this.myGroup = new FormGroup({
+      name: new FormControl(null, [Validators.required, this.testNameNotAllowed.bind(this)], this.test2NameNotAllowed),
+      mail: new FormControl(null, [Validators.required, Validators.email]),
+      status: new FormControl(null)
+    });
+
+    this.myGroup.statusChanges.subscribe(x => {
+      console.log('Status: ' + x);
+    });
+  }
+
+  onSubmit() {
+    console.log(this.myGroup.value);
+    this.myGroup.reset();
+  }
+
+  testNameNotAllowed(control: FormControl): {[s: string]: boolean} {
+    if(control.value === 'Test') {
+      return {'testNameNotAllowed': true};
+    }else
+    {
+        return null;
+    }
+  }
+
+  test2NameNotAllowed(control: FormControl): Promise<any> | Observable<any> {
+      const prom = new Promise<any>((resolve, reject) => {
+        setTimeout(() => {
+          if(control.value === 'Test2') {
+            resolve({'test2NameNotAllowed': true});
+          }else
+          {
+            resolve(null);
+          }
+        }, 5000);
+      });
+      return prom;
+  }
 }
